768050c971dbc8fe34e52b3161f6b63c525addea
Mostafa Rafiki <rafikimostafa@gmail.com>
2024-11-19 23:06:08 +0100

    nice

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-//packages
 import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import { fileURLToPath } from "url";
 
-//utiles
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
@@ -13,7 +12,6 @@ import uplaodRoutes from "./routes/uplaodRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 
 dotenv.config();
-const port = process.env.PORT || 5000;
 
 connectDB();
 
@@ -33,7 +31,10 @@ app.get("/api/config/paypal", (req, res) => {
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID });
 });
 
-const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-app.listen(port, () => console.log(`Server running on port: ${port}`));
+// Remove the app.listen() call
+
+export default app;
